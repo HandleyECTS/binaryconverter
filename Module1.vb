10261a6eada1021e495ff9e0a02db44950c467a4
Logan Handley <CMP_LoHandley@ects.org>
2023-12-08 10:25:52 -0500

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,55 @@
+﻿Module Module1
+    'write a program that prompts the user to enter b to convert a decimal number to binary and
+    'd to convert a binary number to a decimal, q to quit
+    'b d and q in any case should be supported
+    'if the user chooses to convert to binary, make sure a valid integer is given
+    'do not try to convert words
+    'if invalid input is given, report the error and prompt again
+    '{decimal num} converted to decimal is : {binary str}
+    'if the user chooses to convert to decimal from binary, make sure the string given
+    'contains only 0s and 1s. if it contains anything else, report the error and prompt again
+    'when a valid string is given print out the conversion as
+    '{binary str} converted to decimal is : {decimal num}
+    'do not have any subs or functions with more than 25 lines
+    Sub Main()
+        Dim input As String
+        Do
+            Dim valid As Boolean
+            Dim num As Integer
+            Console.Write("Please enter a number, and I'll tell you if it's even or odd! Input Q to quit -> ")
+            input = Console.ReadLine()
+            valid = Integer.TryParse(input, num)
+
+            If valid Then
+                'determine if the num is even or odd
+                If num Mod 2 = 0 Then
+                    Console.WriteLine("It's even!")
+                Else
+                    Console.WriteLine("That's odd.")
+                End If
+            ElseIf input.ToUpper = "Q" Then
+                Console.WriteLine("Come again soon!")
+            Else
+                Console.WriteLine("That's not a number, silly!")
+            End If
+        Loop While Input.toUpper() <> "Q"
+        Console.WriteLine()
+        StringWalkin("hola")
+
+        Console.ReadKey()
+    End Sub
+
+    Sub StringWalkin(somestr As String)
+        'assumes the string is at least 3 characters long
+        Console.WriteLine($"Original String -> {somestr}")
+        Console.WriteLine($"The first character in the string -> {somestr(0)}")
+        Console.WriteLine($"The second character in the string -> {somestr(1)}")
+        Console.WriteLine($"The third character in the string -> {somestr(2)}")
+
+        For i As Integer = 0 To somestr.Length - 1
+            Console.WriteLine(somestr(i))
+        Next
+
+
+    End Sub
+End Module
---
+++ binary.vb
@@ -0,0 +1,85 @@
+﻿Imports System.Xml.Schema
+
+Module binary
+    'Logan Handley
+    '12/07/23
+    'binary-decimal converter
+    Sub main()
+        Dim input As String
+        Do
+            Console.Write("Enter b to convert to binary (base 2), d to convert to base 10, q to quit -> ")
+            input = Console.ReadLine().ToUpper
+            If input = "B" Then
+                Console.WriteLine()
+                dtb()
+            ElseIf input = "D" Then
+                Console.WriteLine()
+                btd()
+            ElseIf input = "Q" Then
+                Console.WriteLine()
+                Console.WriteLine("Come again soon!")
+            Else
+                Console.WriteLine("That's... That's not an option. What does that even mean?" & vbNewLine)
+            End If
+        Loop While input <> "Q"
+
+
+        Console.ReadKey()
+    End Sub
+    Sub dtb()
+        Dim str As String
+        Dim input As String
+        Dim valid As Boolean
+        Dim num As Integer
+        Dim binstr As String = ""
+        Console.WriteLine("Decimal to binary it is!")
+        Console.Write("Please enter the number you want to convert to binary -> ")
+        input = Console.ReadLine()
+        valid = Integer.TryParse(input, num)
+        If valid = True Then
+            Do
+                str = num Mod 2
+                binstr = str & binstr
+                num = Math.Floor(num / 2)
+            Loop While num > 0
+            Console.WriteLine($"{input} converted to binary is {binstr}." & vbNewLine)
+        Else
+            Console.WriteLine("That's not a number, silly!")
+        End If
+    End Sub
+    Sub btd()
+        Dim total As Integer
+        Dim input As String
+        Dim num As String
+        Dim decstr As String = ""
+        Console.WriteLine("Alright, binary to decimal we go!")
+        Console.Write("Please enter the binary you want to convert to a decimal -> ")
+        input = Console.ReadLine()
+        isValidBinary(input)
+        If isValidBinary(input) = True Then
+            num = StrReverse(input)
+            For i As Integer = 0 To num.Length - 1
+                Dim bit As Integer
+                Integer.TryParse(num(i), bit)
+                total += bit * 2 ^ i
+            Next
+            Console.WriteLine($"{input} converted to decimal is {total}." & vbNewLine)
+        Else
+            Console.WriteLine("That's not binary, silly!" & vbNewLine)
+        End If
+    End Sub
+    ''' <summary>
+    ''' accepts a string and checks it to make sure it contains only 1s and 0s.
+    ''' </summary>
+    ''' <param name="str"></param>
+    ''' <returns></returns>
+    Function isValidBinary(str As String) As Boolean
+        Dim valid As Boolean = True
+        For i As Integer = 0 To str.Length - 1
+            If str(i) <> "0" AndAlso str(i) <> "1" Then
+                valid = False
+            End If
+        Next
+        Return valid
+    End Function
+End Module
